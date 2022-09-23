6dd10395f2effb3de8d90103152f9b12d18bf913
yuehuass <114207259+yuehuass@users.noreply.github.com>
2022-09-23 13:29:20 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ export const USER_CONFIG = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "",
+  APP_ID: "wx94552fa0e915b4c4",
   
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "",
+  APP_SECRET: "fa460fb12346dc002d5b59fedd99a464",
   
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -62,9 +62,9 @@ export const USER_CONFIG = {
       // 想要发送的人的名字
       name: "老婆0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "ockGO50gmqonSrdkpx1BxH5f2MWk",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",
+      useTemplateId: "IFtcGkbT_Fp9EAs0GLsyyBu1UAoi-hlfemjN2E9YvKs",
       // 所在省份
       province: "广东",
       // 所在城市
